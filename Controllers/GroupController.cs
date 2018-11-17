dbd759802e75e1c6c7f4ff28bb462b8a0536a1a0
Selim <yqszt@qq.com>
2018-11-17 09:07:26 +0800

    create a demo

---
+++ Controllers/GroupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaDemo.Controllers
+{
+    [Area("Manage")]
+    public class GroupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
